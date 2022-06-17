8a92ea65a65360d25ec42d9a94d67b36778da027
Austin Reed <97327532+Aussymo@users.noreply.github.com>
2022-06-17 09:28:03 -0600

    errors

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,6 @@ import {
   faJsSquare,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
-import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
@@ -73,7 +72,6 @@ const About = () => {
           </div>
         </div>
       </div>
-      <Loader type="pacman" />
     </>
   )
 }
